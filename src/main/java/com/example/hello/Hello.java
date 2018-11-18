60ef4a35a71fbcb6a10b40a5c7a425062966fca0
allAutomation <44010216+allAutomation@users.noreply.github.com>
2018-11-17 21:08:14 -0500

    Update Hello.java

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -5,5 +5,9 @@ public class Hello {
 	public static void main(String [] args) {
 		System.out.println("Hello, world!");
 	}
+	@Test
+	public void testOne(){
+		System.out.println("Hello, world!");		
+	}
 	
 }
